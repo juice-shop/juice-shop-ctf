98f52a5206ef6431d81d6b74e1b7a44c14ab2d96
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-02-16 12:44:20 +0100

    Split responsibilities (while introducing Promises)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+var Promise = require('bluebird')
 var inquirer = require('inquirer')
 var request = require('request-promise')
 var jsSHA = require('jssha')
@@ -8,9 +9,6 @@ var path = require('path')
 var colors = require('colors') // eslint-disable-line no-unused-vars
 
 var juiceShopCtfCli = function () {
-  console.log()
-  console.log('Generate INSERT statements for ' + 'CTFd'.bold + ' with the ' + 'OWASP Juice Shop'.bold + ' challenges')
-
   var questions = [
     {
       type: 'input',
@@ -38,70 +36,96 @@ var juiceShopCtfCli = function () {
     }
   ]
 
+  console.log()
+  console.log('Generate INSERT statements for ' + 'CTFd'.bold + ' with the ' + 'OWASP Juice Shop'.bold + ' challenges')
   inquirer.prompt(questions).then(function (answers) {
     console.log()
-    if (answers.ctfKey && answers.ctfKey.match(/.*:\/\//)) {
-      request(answers.ctfKey)
-      .then(function (body) {
-        answers.ctfKey = body
-        generateSql(answers)
-      }).catch(function (error) {
-        console.log('Failed'.red + ' to fetch HMAC key from URL! ' + error)
+    fetchHmacKey(answers.ctfKey).then(function (hmacKey) {
+      fetchChallenges(answers.juiceShopUrl).then(function (challenges) {
+        generateSql(challenges, answers.deleteBeforeInsert, answers.selectAfterInsert, hmacKey).then(function (sql) {
+          writeOutput(sql)
+        })
       })
+    })
+  })
+}
+
+function fetchHmacKey (ctfKey) {
+  return new Promise(function (resolve) {
+    if (ctfKey && isUrl(ctfKey)) {
+      request(ctfKey)
+        .then(function (body) {
+          resolve(body)
+        }).catch(function (error) {
+          console.log('Failed'.red + ' to fetch HMAC key from URL! ' + error)
+          process.exit()
+        })
     } else {
-      generateSql(answers)
+      resolve(ctfKey)
     }
   })
 }
 
-function generateSql (answers) {
-  request({ url: answers.juiceShopUrl + '/api/Challenges', json: true })
-    .then(function (json) {
-      var sql = ''
-      if (answers.deleteBeforeInsert) {
-        sql = sql + 'DELETE FROM challenges;\n\r'
-      }
-      /* This is a default multiplier whereby the score will be = (difficulty x multiplier)
-       i.e. using these multipliers will create challenges with the following scores:
-       *     =  100
-       **    =  250
-       ***   =  450
-       ****  =  700
-       ***** = 1000
-       I think it is fair to assume that completing a 5* task will be 10 times harder than a 1* task.
-       */
-      var multiplier = [ 100, 125, 150, 175, 200 ]
-      var challenges = json.data
-      var bar = new ProgressBar('Generating INSERT statements [:bar] :percent', { total: Object.keys(challenges).length })
-      for (var key in challenges) {
-        bar.tick()
-        if (challenges.hasOwnProperty(key)) {
-          var challenge = challenges[key]
-          sql = sql + 'INSERT INTO challenges (id, name, description, value, category, flags, hidden) VALUES ('
-          sql = sql + '' + challenge.id + ', '
-          sql = sql + '"' + challenge.name + '", '
-          sql = sql + '"' + challenge.description.replace(/"/g, '""') + ' (Difficulty Level: ' + challenge.difficulty + ')", '
-          sql = sql + '"' + challenge.difficulty * multiplier[ challenge.difficulty - 1 ] + '", '
-          sql = sql + '"' + challenge.category + '", '
-          sql = sql + '"[{""flag"": ""' + toHmac(challenge.name, answers.ctfKey) + '"", ""type"": 0}]", '
-          sql = sql + '0);\n\r'
-        }
-      }
-      if (answers.selectAfterInsert) {
-        sql = sql + 'SELECT * FROM challenges;\n\r'
-      }
-      fs.writeFile('insert-ctfd-challenges.sql', sql, function (error) {
-        if (error) {
-          return console.log('Failed'.red + ' to write output to file! ' + error)
-        }
-        console.log('SQL written to ' + path.resolve('insert-ctfd-challenges.sql').green)
-        console.log()
-        console.log('For a step-by-step guide to apply the INSERT statements to ' + 'CTFd'.bold + ', please refer to')
-        console.log('https://github.com/bkimminich/juice-shop-ctf/blob/master/CTFd/GenerateCTFdInserts.html#L80'.gray) // TODO Refer to Markdown doc on GitHub instead
-      })
+function fetchChallenges (juiceShopUrl) {
+  return new Promise(function (resolve) {
+    request({ url: juiceShopUrl + '/api/Challenges', json: true }).then(function (body) {
+      resolve(body.data)
     }).catch(function (error) {
       console.log('Failed'.red + ' to fetch challenges from API! ' + error)
+      process.exit()
     })
+  })
+}
+
+function generateSql (challenges, prependDelete, appendSelect, hmacKey) {
+  return new Promise(function (resolve) {
+    var sql = ''
+    if (prependDelete) {
+      sql = sql + 'DELETE FROM challenges;\n\r'
+    }
+    /* This is a default multiplier whereby the score will be = (difficulty x multiplier)
+     i.e. using these multipliers will create challenges with the following scores:
+     *     =  100
+     **    =  250
+     ***   =  450
+     ****  =  700
+     ***** = 1000
+     I think it is fair to assume that completing a 5* task will be 10 times harder than a 1* task.
+     */
+    var multiplier = [ 100, 125, 150, 175, 200 ]
+    var bar = new ProgressBar('Generating INSERT statements [:bar] :percent', { total: Object.keys(challenges).length })
+    for (var key in challenges) {
+      bar.tick()
+      if (challenges.hasOwnProperty(key)) {
+        var challenge = challenges[key]
+        sql = sql + 'INSERT INTO challenges (id, name, description, value, category, flags, hidden) VALUES ('
+        sql = sql + '' + challenge.id + ', '
+        sql = sql + '"' + challenge.name + '", '
+        sql = sql + '"' + challenge.description.replace(/"/g, '""') + ' (Difficulty Level: ' + challenge.difficulty + ')", '
+        sql = sql + '"' + challenge.difficulty * multiplier[ challenge.difficulty - 1 ] + '", '
+        sql = sql + '"' + challenge.category + '", '
+        sql = sql + '"[{""flag"": ""' + toHmac(challenge.name, hmacKey) + '"", ""type"": 0}]", '
+        sql = sql + '0);\n\r'
+      }
+    }
+    if (appendSelect) {
+      sql = sql + 'SELECT * FROM challenges;\n\r'
+    }
+    resolve(sql)
+  })
+}
+
+function writeOutput (sql) {
+  fs.writeFile('insert-ctfd-challenges.sql', sql, function (error) {
+    if (error) {
+      console.log('Failed'.red + ' to write output to file! ' + error)
+      process.exit()
+    }
+    console.log('SQL written to ' + path.resolve('insert-ctfd-challenges.sql').green)
+    console.log()
+    console.log('For a step-by-step guide to apply the INSERT statements to ' + 'CTFd'.bold + ', please refer to')
+    console.log('https://github.com/bkimminich/juice-shop-ctf/blob/master/CTFd/GenerateCTFdInserts.html#L80'.gray) // TODO Refer to Markdown doc on GitHub instead
+  })
 }
 
 function toHmac (text, theSecretKey) {
@@ -109,6 +133,10 @@ function toHmac (text, theSecretKey) {
   shaObj.setHMACKey(theSecretKey, 'TEXT')
   shaObj.update(text)
   return shaObj.getHMAC('HEX')
+}
+
+function isUrl (text) {
+  return text.match(/.*:\/\//)
 }
 
 exports.juiceShopCtfCli = juiceShopCtfCli
