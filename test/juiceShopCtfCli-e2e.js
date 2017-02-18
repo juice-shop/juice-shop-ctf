3a3494ce9dc42910aebd143277867834e06ce047
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2017-02-18 23:09:16 +0100

    Extend e2e tests (running with unit tests for now)

--- test/juiceShopCtfCli-e2e.js
+++ test/juiceShopCtfCli-e2e.js
@@ -11,7 +11,30 @@ const juiceShopCtfCli = path.join(__dirname, '../bin/juice-shop-ctf.js')
 describe('juiceShopCtfCli()', function () {
   it('should accept defaults for all input questions', function (done) {
     this.timeout(20000)
-    expect(run(juiceShopCtfCli, [ENTER, ENTER, ENTER, ENTER], 1000)).to.eventually.match(/SQL written to/).and.notify(done)
+    expect(run(juiceShopCtfCli, [ENTER, ENTER, ENTER, ENTER], 1000)).to
+      .eventually.match(/SQL written to/i).and
+      .eventually.match(/DELETE all CTFd Challenges before INSERT statements\? Yes/i).and
+      .eventually.match(/SELECT all CTFd Challenges after INSERT statements\? Yes/i).and
+      .notify(done)
+  })
+
+  it('should fail on invalid Juice Shop URL', function (done) {
+    this.timeout(20000)
+    expect(run(juiceShopCtfCli, ['localhorst', ENTER, ENTER, ENTER, ENTER], 1000)).to.eventually.match(/Failed to fetch challenges from API!/i).and.notify(done)
+  })
+
+  it('should fail on invalid ctf.key URL', function (done) {
+    this.timeout(20000)
+    expect(run(juiceShopCtfCli, [ENTER, 'http://i.n.v.a.l.i.d/ctf-key', ENTER, ENTER, ENTER], 1000)).to.eventually.match(/Failed to fetch secret key from URL!/i).and.notify(done)
+  })
+
+  it('should not insert DELETE statement when not chosen', function (done) {
+    this.timeout(20000)
+    expect(run(juiceShopCtfCli, [ENTER, ENTER, 'n', ENTER, ENTER], 1000)).to.eventually.match(/DELETE all CTFd Challenges before INSERT statements\? No/i).and.notify(done)
+  })
+
+  it('should not insert SELECT statement when not chosen', function (done) {
+    this.timeout(20000)
+    expect(run(juiceShopCtfCli, [ENTER, ENTER, ENTER, 'n', ENTER], 1000)).to.eventually.match(/SELECT all CTFd Challenges after INSERT statements\? No/i).and.notify(done)
   })
 })
-
