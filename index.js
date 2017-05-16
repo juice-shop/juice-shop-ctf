b613b879b26835890624bf281fcca021e89d25bf
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-05-16 17:31:05 +0200

    Change text color (to be more asciinema-friendly)

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ var juiceShopCtfCli = function () {
             console.log('SQL written to ' + file)
             console.log()
             console.log('For a step-by-step guide to apply the INSERT statements to ' + 'CTFd'.bold + ', please refer to')
-            console.log('https://github.com/bkimminich/juice-shop-ctf#populating-the-ctfd-database'.gray)
+            console.log('https://github.com/bkimminich/juice-shop-ctf#populating-the-ctfd-database'.bold)
           }, function (error) {
             console.log(error.red)
           })
