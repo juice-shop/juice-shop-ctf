4bc6260aeae7c852dcf3c9ffd327cde88eff8948
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2017-06-22 17:28:30 +0200

    Refactor hmac order of parameters

--- test/unit/hmac-spec.js
+++ test/unit/hmac-spec.js
@@ -1,17 +1,17 @@
 var chai = require('chai')
 var expect = chai.expect
-var hmac = require('../../lib/hmac')
+var hmacSha1 = require('../../lib/hmac')
 
 describe('Flag', function () {
   it('should be a SHA256 HMAC from plain text and given secret key', function () {
-    expect(hmac('text', 'ZJnHOTckstBeJP!QC2T')).to.equal('5188938396d17832dd26fbabd5ac4d79518a87da')
+    expect(hmacSha1('ZJnHOTckstBeJP!QC2T', 'text')).to.equal('5188938396d17832dd26fbabd5ac4d79518a87da')
   })
 
   it('should be a SHA256 HMAC from plain text and empty secret key', function () {
-    expect(hmac('text', '')).to.equal('f3f276f5001644013dbd202f1854bd61f9123d0f')
+    expect(hmacSha1('', 'text')).to.equal('f3f276f5001644013dbd202f1854bd61f9123d0f')
   })
 
   it('should be different for different plain texts with same secret key', function () {
-    expect(hmac('text1', 'key')).to.not.equal(hmac('text2', 'key'))
+    expect(hmacSha1('key', 'text1')).to.not.equal(hmacSha1('key', 'text2'))
   })
 })
