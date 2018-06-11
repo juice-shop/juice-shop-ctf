270037a6bd5b81df19e58d47ec1a1e4bebf81367
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2018-06-11 18:36:45 +0200

    Fix Stryker config as far as possible

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,10 +1,10 @@
 module.exports = function (config) {
   config.set({
-    files: [
-      { pattern: 'index.js', mutated: false, included: false },
-      { pattern: 'lib/**/*.js', mutated: true, included: false },
-      'test/unit/*-spec.js'
-    ],
+    mutate: ['lib/**/*.js'],
+    files: ['index.js', 'lib/**/*.js', 'test/unit/*-spec.js'],
+    mochaOptions: {
+      files: ['test/unit/**/*-spec.js']
+    },
     testRunner: 'mocha',
     testFramework: 'mocha',
     coverageAnalysis: 'perTest',
