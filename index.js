ed652b6137dd584457c52d25433975490e9577b2
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2019-01-29 10:47:35 +0100

    Add CTFd 2.x to intro CLI printout

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function getConfig (argv, questions) {
 
 const juiceShopCtfCli = async () => {
   console.log()
-  console.log('Generate ' + 'OWASP Juice Shop'.bold + ' challenge archive for setting up ' + options.ctfdFramework.bold + ' or ' + options.fbctfFramework.bold + ' score server')
+  console.log('Generate ' + 'OWASP Juice Shop'.bold + ' challenge archive for setting up ' + options.ctfdFramework.bold + ', ' + options.ctfd2Framework.bold + ' or ' + options.fbctfFramework.bold + ' score server')
 
   try {
     const answers = await getConfig(argv, questions)
