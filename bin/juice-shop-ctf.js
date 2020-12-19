8142dc11ef40c7f01a0318c7692d0c36a2517c1e
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2020-12-19 23:17:03 +0100

    Fix syntactical order of headers

--- bin/juice-shop-ctf.js
+++ bin/juice-shop-ctf.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) 2016-2021 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
 
-#!/usr/bin/env node
 const juiceShopCtfCli = require('../index')
 
 juiceShopCtfCli()
