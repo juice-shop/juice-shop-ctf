a16b5fd121ba78c692cf6aa12af6a1baee3e06d3
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2021-09-20 16:57:56 +0200

    Fix spacing in all copyright year spans#

--- test/unit/hmac-spec.js
+++ test/unit/hmac-spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 -2021 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * Copyright (c) 2016-2021 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
 
