d6cab9bd03b226b92209c828834e54258b090106
Harsh Kumar <harshmastic@gmail.com>
2025-07-02 23:05:12 +0530

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,26 @@
 module.exports = {
   extends: [
-    "standard-with-typescript",
+    'standard-with-typescript'
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   env: {
-    node: true,
+    node: true
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    project: "./tsconfig.json",
+    project: './tsconfig.json'
   },
   ignorePatterns: [
-    ".eslintrc.js",
-    "build/**",
-    "node_modules/**",
-    ".nyc_output/**",
-    "dist/**",
-    ""
-  ],
-};
+    '.eslintrc.js',
+    'build/**',
+    'node_modules/**',
+    '.nyc_output/**',
+    'dist/**',
+    ''
+  ]
+}
